34971246d82a63b65514208a1ff80aa2350abcd5
abdukhans <abdullahmoves@gmail.com>
2025-05-29 17:46:42 -0400

    made the README.md more helpful and updated both tests.py and .gitignore

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 ## How to use this repo
 
 This repo is meant only to test the code in the [LithoConverter](https://github.com/abdukhans/LithoConverter) library.
+
+## How to run the code
+The code needs the ``LITHO_CSVS`` folder and the ``Data`` folder. The ``Data`` folder is not included in this repo so you must create it. 
+
+First step is to create an empty ``Data`` folder, in the same working directory as ``tests.py``.
+
+Next you need to download the actual data itself. You need to get this data from the mineral google drive. Below are links to the datasets in the google drive. Download each of the files below and put it in the ``Data`` folder since that is where ``tests.py`` assumes they will be.
+
+* [British Columbia Dataset](https://drive.google.com/file/d/1w3o7Kgcfr0BVokaCVfzc_IMppXZIdzE-/view?usp=drive_link)
+
+* [Ontario Dataset](https://drive.google.com/file/d/1xmbvik3PC9nESo1hC68uM5fkDH17h8gA/view?usp=drive_link)
+
+* [USGS Dataset](https://drive.google.com/file/d/1-W5O5qW9ftCAW5eLzJcKlzgiQzSy1sli/view?usp=drive_link)
+
+* [SARIG Keyword Dataset](https://drive.google.com/file/d/1CQnFdJmVR4dJDl_MhEMRwk22crEjLKuv/view?usp=drive_link)
+
+
+## Warning !
+Running the ``tests.py`` may end up crashing your system. When running this on my machine it took up an extra 17GB of RAM!!!!!!!!!! This is why it just crashes on machines that don't have as much memory. This is clearly a problem that needs to be fixed. If you want to run this script you may have to reduce the number of rows the  BinVectorMarker generates on. To do this  I would recommend commenting out lines 25 to 31 inclusive. This reduces the number of sample descriptions rows that it translates to binary vectors to 10,000. Uncommenting these lines makes the number of rows it has to translate 1.3 million!!!! which may just crash your system, so just proceed with caution.    
--- tests.py
+++ tests.py
@@ -1,16 +1,21 @@
 from LithoConverter import BinVectorMarker
 from LithoConverter import gen_similar_word_freq_csv,open_csv_df,gen_eng_compressed_csv,gen_llm_word_def_csv
 from LithoConverter import gen_csvs
+import pandas as pd
 import os 
 
 
 if __name__ == "__main__":
-    CWD             = os.getcwd()
-    BRIT_DF_PATH    = os.path.join(CWD,"British Columbia","Lithogeochemical","lithogeochem_data.csv")
-  
-    brit_df       = open_csv_df(BRIT_DF_PATH)
-    
+    CWD             = os.getcwd()  
+    BRIT_DF_PATH    = os.path.join(CWD,"Data","lithogeochem_data.csv")
+    USGS_DF_PATH    = os.path.join(CWD,"Data","USGS_rocks_with_lithology_data.csv")
+    SARGI_DF_PATH   = os.path.join(CWD,"Data","rock_lith_mod_counts_sarig.csv")
+    ONTARIO_DF_PATH = os.path.join(CWD,"Data", "Ontario_rock.csv")
 
+    brit_df       = open_csv_df(BRIT_DF_PATH)
+    usgs_df       = open_csv_df(USGS_DF_PATH)
+    sarig_df      = open_csv_df(SARGI_DF_PATH)
+    ontario_df    = open_csv_df(ONTARIO_DF_PATH)
 
 
 
@@ -30,7 +35,6 @@ if __name__ == "__main__":
     print(df.shape)
     csv_out_folder = 'LITHO_CSVS'
 
-
     if not(os.path.exists(csv_out_folder)):
         gen_csvs(df,out_dir=csv_out_folder)
     
@@ -44,6 +48,9 @@ if __name__ == "__main__":
     BIN_VEC_MARKER = BinVectorMarker(sim_key_word_csv_path,word_def_csv_path)
 
 
+
+    
+
     bin_vecs= BIN_VEC_MARKER.gen_bin_vecs(df).apply(lambda x: ','.join(map(str, x)))
 
 
@@ -54,17 +61,17 @@ if __name__ == "__main__":
 
 
     
-    sim_key_word_csv_path = os.path.join(CWD,csv_out_folder,"similar_keywords_compressed_freq.csv")
-    word_def_csv_path     = os.path.join(CWD,csv_out_folder,"word_def.csv")
+    # sim_key_word_csv_path = os.path.join(CWD,csv_out_folder,"similar_keywords_compressed_freq.csv")
+    # word_def_csv_path     = os.path.join(CWD,csv_out_folder,"word_def.csv")
 
 
-    BIN_VEC_MARKER = BinVectorMarker(sim_key_word_csv_path,word_def_csv_path)
+    # BIN_VEC_MARKER = BinVectorMarker(sim_key_word_csv_path,word_def_csv_path)
 
 
-    bin_vecs= BIN_VEC_MARKER.gen_bin_vecs(df).apply(lambda x: ','.join(map(str, x)))
+    # bin_vecs= BIN_VEC_MARKER.gen_bin_vecs(df).apply(lambda x: ','.join(map(str, x)))
 
 
-    bin_vec_df = pd.concat([df,bin_vecs],keys=['Sample_Descr','Bin_Vec'],axis=1)
+    # bin_vec_df = pd.concat([df,bin_vecs],keys=['Sample_Descr','Bin_Vec'],axis=1)
 
-    bin_vec_df.to_csv("bin_vec.csv")
+    # bin_vec_df.to_csv("bin_vec.csv")
 
